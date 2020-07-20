312c30de7291e70261728405a915491441f7187e
unknown <davidhale87@gmail.com>
2020-07-20 09:21:59 -0500

    Updating to test on the website.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { type } = require("os");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 35000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -16,19 +16,16 @@ app.use(express.json());
 app.use(express.static('public'))
 app.use(express.static('db'))
 
-var notes =[
-  {
-    textTitle: "Test Title",
-    text: "Note"
-  }
-  
-];
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
   });
 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
+  });
+
+  app.get("/test", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/test.html"));
   });
 
 app.get("/api/notes", function(req, res) {
